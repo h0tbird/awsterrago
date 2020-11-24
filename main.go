db7b53fb6b042f75c6daf1e1980fd57874980cf1
Marc Villacorta <mvillacorta@newrelic.com>
2020-11-24 23:32:30 +0100

    Adds context and region

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 
 	// stdlib
+	"context"
 	"fmt"
 
 	// terraform
@@ -12,15 +13,18 @@ import (
 
 func main() {
 
+	ctx := context.Background()
 	provider := aws.Provider()
 
 	rc := &terraform.ResourceConfig{
 		ComputedKeys: []string{},
 		Raw:          map[string]interface{}{},
-		Config:       map[string]interface{}{},
+		Config: map[string]interface{}{
+			"region": "us-east-2",
+		},
 	}
 
-	provider.Configure(nil, rc)
+	provider.Configure(ctx, rc)
 
 	dataSources := provider.DataSources()
 	for _, v := range dataSources {
