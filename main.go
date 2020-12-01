80ae85b5000245ddc0d0774aeeb7922bf3f50744
Marc Villacorta <mvillacorta@newrelic.com>
2020-12-01 23:03:11 +0100

    Create a S3 bucket

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
+//----------------------------------------------------------------
+//
+//----------------------------------------------------------------
+
 import (
 
 	// stdlib
 	"context"
 	"fmt"
+	"os"
 
 	// terraform
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/terraform-providers/terraform-provider-aws/aws"
 )
+
+//----------------------------------------------------------------
+//
+//----------------------------------------------------------------
 
 func main() {
 
@@ -23,17 +32,24 @@ func main() {
 		},
 	})
 
-	// List all data sources
-	fmt.Println("--[Data sources]----------------")
-	dataSources := provider.DataSources()
-	for _, v := range dataSources {
-		fmt.Println(v.Name)
+	// Configure the resource
+	resourceConfig := &terraform.ResourceConfig{
+		Config: map[string]interface{}{
+			"bucket": "my-nice-bucket",
+		},
 	}
 
-	// List all resources
-	fmt.Println("--[Resources]----------------")
-	resources := provider.Resources()
-	for _, v := range resources {
-		fmt.Println(v.Name)
+	instanceState := &terraform.InstanceState{}
+	AWSS3Bucket := provider.ResourcesMap["aws_s3_bucket"]
+
+	// Diff
+	instanceDiff, err := AWSS3Bucket.Diff(ctx, instanceState, resourceConfig, provider.Meta())
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(0)
 	}
+
+	// Apply
+	istate, _ := AWSS3Bucket.Apply(ctx, instanceState, instanceDiff, provider.Meta())
+	fmt.Println(istate)
 }
