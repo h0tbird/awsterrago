56211771223f8cbc9d6a9b41ac1178ebfb350ed3
Marc Villacorta <mvillacorta@newrelic.com>
2020-12-09 23:38:08 +0100

    Log with logrus

--- main.go
+++ main.go
@@ -8,10 +8,12 @@ import (
 
 	// stdlib
 	"context"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
+
+	// community
+	"github.com/sirupsen/logrus"
 
 	// terraform
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
@@ -33,6 +35,7 @@ func main() {
 	p := aws.Provider()
 
 	// Configure the provider
+	logrus.Info("Configuring the provider...")
 	diags := p.Configure(ctx, &terraform.ResourceConfig{
 		Config: map[string]interface{}{
 			"region": "us-east-2",
@@ -42,12 +45,13 @@ func main() {
 	if diags != nil && diags.HasError() {
 		for _, d := range diags {
 			if d.Severity == diag.Error {
-				fmt.Printf("error configuring the provider: %s", d.Summary)
+				logrus.Fatalf("error configuring the provider: %s", d.Summary)
 			}
 		}
 	}
 
 	// Configure the resource
+	logrus.Info("Configuring the resource...")
 	resourceConfig := &terraform.ResourceConfig{
 		Config: map[string]interface{}{
 			"bucket": "my-nice-bucket",
@@ -67,46 +71,48 @@ func main() {
 	AWSS3Bucket := p.ResourcesMap["aws_s3_bucket"]
 
 	// Refresh
+	logrus.Info("Refreshing the state...")
 	state1, diags := AWSS3Bucket.RefreshWithoutUpgrade(ctx, state0, p.Meta())
 	if diags != nil && diags.HasError() {
 		for _, d := range diags {
 			if d.Severity == diag.Error {
-				fmt.Printf("error reading the instance state: %s", d.Summary)
+				logrus.Fatalf("error reading the instance state: %s", d.Summary)
 			}
 		}
 	}
 
 	// Diff
+	logrus.Info("Diffing state and config...")
 	diff1, err := AWSS3Bucket.Diff(ctx, state1, resourceConfig, p.Meta())
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(0)
+		logrus.Fatal(err)
 	}
 
 	if diff1 == nil {
-		fmt.Println("All good 1")
+		logrus.Info("All good!")
 		os.Exit(0)
 	}
 
 	// Apply
+	logrus.Info("Applying changes...")
 	state2, diags := AWSS3Bucket.Apply(ctx, state1, diff1, p.Meta())
 	if diags != nil && diags.HasError() {
 		for _, d := range diags {
 			if d.Severity == diag.Error {
-				fmt.Printf("error configuring S3 bucket: %s", d.Summary)
+				logrus.Fatalf("error configuring S3 bucket: %s", d.Summary)
 			}
 		}
 	}
 
 	// Diff
+	logrus.Info("Diffing state and config...")
 	diff2, err := AWSS3Bucket.Diff(ctx, state2, resourceConfig, p.Meta())
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(0)
+		logrus.Fatal(err)
 	}
 
 	if diff2 == nil {
-		fmt.Println("All good 2")
+		logrus.Info("All good!")
 		os.Exit(0)
 	}
 }
