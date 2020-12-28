ad9896be014945f36e9b420b61357d838dc918fc
Marc Villacorta <mvillacorta@newrelic.com>
2020-12-28 23:12:29 +0100

    Move state to state.go

--- main.go
+++ main.go
@@ -7,13 +7,9 @@ package main
 import (
 
 	// stdlib
-	"bytes"
 	"context"
-	"encoding/json"
-	"io"
 	"io/ioutil"
 	"log"
-	"os"
 
 	// community
 	"github.com/sirupsen/logrus"
@@ -27,50 +23,6 @@ import (
 	"github.com/h0tbird/awsterrago/pkg/dag"
 	"github.com/h0tbird/awsterrago/pkg/resource"
 )
-
-//-----------------------------------------------------------------------------
-// State implementation
-//-----------------------------------------------------------------------------
-
-type state struct{}
-
-func (s *state) Read(logicalID string, state interface{}) error {
-
-	// Open a file handler
-	f, err := os.Open(os.Getenv("HOME") + "/.terramorph/" + logicalID + ".json")
-	if err != nil {
-
-		// No file means no state
-		if os.IsNotExist(err) {
-			return nil
-		}
-		return err
-	}
-	defer f.Close()
-
-	// Unmarshal json
-	return json.NewDecoder(f).Decode(state)
-}
-
-func (s *state) Write(logicalID string, state interface{}) error {
-
-	// Open a file handler
-	f, err := os.Create(os.Getenv("HOME") + "/.terramorph/" + logicalID + ".json")
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	// Marshal json
-	jsonBytes, err := json.MarshalIndent(state, "", "  ")
-	if err != nil {
-		return err
-	}
-
-	// Write to disk
-	_, err = io.Copy(f, bytes.NewReader(jsonBytes))
-	return err
-}
 
 //-----------------------------------------------------------------------------
 // Init
